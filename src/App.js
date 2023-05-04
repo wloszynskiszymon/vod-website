5e8602e7fe88423559d487f2482154c9617d1a15
wloszynskiszymon <ace3szymon@gmail.com>
2023-05-04 21:54:42 +0200

    Adding rest of the layout
    Adding rest of the layout, I'll update it later

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import NavBar from './components/NavBar';
 import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <NavBar />
       <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
