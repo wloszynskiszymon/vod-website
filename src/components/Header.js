098656038fcb0d2376b43c40dc1c5a37deb21749
Szymon Włoszyński <66365050+wloszynskiszymon@users.noreply.github.com>
2023-05-06 19:30:33 +0200

    Starting RWD and bugfixes
    Starting RWD and bugfixes + moving code to PC from laptop

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,20 @@ import InputSearch from './UI/InputSearch';
 
 const Header = (props) => {
   return (
-    <header className='h-[45rem] relative bg-gray-900'>
+    <header className='h-[90vh] min-h-[30rem] relative bg-gray-900'>
       <img
         src={props.img}
         alt='Header background'
-        className='w-5/6 h-full absolute transfrom top-0 left-1/2 -translate-x-1/2 bg-cover bg-center'
+        className='w-full lg:w-5/6 h-full absolute transfrom top-0 left-1/2 -translate-x-1/2 bg-cover bg-center md:bg-contain'
       />
       <div
-        className={`m-auto w-11/12 h-full px-40 z-10 relative bg-gradient-to-r from-gray-900 from-10% via-[rgba(17,24,39,0.6)] via-50% to-gray-900 to-90%`}
+        className={`m-auto w-full lg:w-11/12 h-full px-0 lg:px-20 xl:px-40 z-10 relative bg-gradient-to-r from-gray-900 from-10% md:from-3% via-[rgba(17,24,39,0.6)] via-50% to-gray-900 to-90% md:to-97%`}
       >
         <div className='flex flex-col justify-end items-center h-1/2'>
-          <h1 className='text-6xl font-extrabold text-white mb-4'>
+          <h1 className='text-3xl md:text-4xl lg:text-6xl font-extrabold text-white mb-4'>
             Witamy na stronie!
           </h1>
-          <p className='mb-10 text-4xl text-gray-300'>
+          <p className='mb-10 text-xl md:text-2xl lg:text-4xl text-gray-300'>
             Znajdź najlepsze{' '}
             <span className='text-purple-600 font-bold'>filmy!</span>
           </p>
--- src/components/UI/MovieTilesRow.js
+++ src/components/UI/MovieTilesRow.js
@@ -2,7 +2,6 @@ import MovieTile from './MovieTile';
 
 const MovieTilesRow = (props) => {
   const isMovie = props.isMovie;
-  const headingTitle = `Najpopularniejsze ${isMovie ? 'filmy' : 'seriale'}`;
 
   const movieTiles = props.data.map((data) => {
     return (
@@ -15,17 +14,17 @@ const MovieTilesRow = (props) => {
             ? `https://image.tmdb.org/t/p/w500/${data.backdrop_path}`
             : null
         }
-        title={isMovie ? data.title : data.name}
+        title={data?.title || data?.name}
       />
     );
   });
 
   return (
     <section className='w-full flex flex-col items-center justify-center'>
-      <h2 className='text-white font-extrabold self-start text-2xl uppercase mt-6 mb-2 px-40 tracking-wider'>
-        {headingTitle}
-      </h2>
-      <div className='px-40 w-full grid grid-cols-5 gap-4 py-2'>
+      {!props.hideHeading && <h2 className='text-white font-extrabold self-start text-2xl uppercase mt-6 mb-2 px-40 tracking-wider'>
+        {`Najpopularniejsze ${isMovie ? 'filmy' : 'seriale'}`}
+      </h2>}
+      <div className='px-40 lg:px-20 md:px-2 w-full grid grid-cols-5 gap-4 md:gap-2 py-2'>
         {movieTiles}
       </div>
     </section>
