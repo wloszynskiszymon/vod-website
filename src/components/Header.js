2c68cf66c90be43d15aeacf086d34c6686e98cb7
wloszynskiszymon <ace3szymon@gmail.com>
2023-05-07 23:42:54 +0200

    Adding remaining functionality, handling errors, loading and RWD
    Adding remaining functionality, handling errors, loading and RWD. The product is ready

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,57 @@
+import { useState, useEffect } from 'react';
+import { API_KEY, useMedia } from '../hooks/useMedia';
 import InputSearch from './UI/InputSearch';
 
-const Header = (props) => {
+import MovieTilesRow from './MovieTiles/MovieTilesRow';
+
+const API_URL_HEADER = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc&with_media_type=movie&region=pl`;
+
+const Header = () => {
+  const [image, setImage] = useState(null);
+  const { data } = useMedia(API_URL_HEADER, 10);
+
+  const [searchResults, setSearchResults] = useState(null);
+  const [, setSearchError] = useState(false);
+
+  useEffect(() => {
+    if (data) {
+      const index = Math.floor(Math.random() * data.length);
+      setImage(
+        `https://image.tmdb.org/t/p/original/${data[index].backdrop_path}`
+      );
+    }
+  }, [data]);
+
   return (
-    <header id='start' className='h-[35rem] lg:h-[45rem] relative bg-gray-900'>
+    <header id='start' className='relative bg-gray-900 flex-center w-full'>
       <img
-        src={props.img}
+        src={image && image}
         alt='Header background'
         className='w-full lg:w-5/6 h-full absolute transfrom top-0 left-1/2 -translate-x-1/2 bg-center object-cover'
       />
       <div
-        className={`m-auto w-full lg:w-11/12 h-full px-0 z-10 relative bg-gradient-to-r from-gray-900 from-10% md:from-3% via-[rgba(17,24,39,0.6)] via-50% to-gray-900 to-90% md:to-97%`}
+        className={`flex-center flex-col w-full lg:w-11/12 h-full z-10 relative bg-gradient-to-r from-[rgba(17,24,39,0.6)] from-10% via-[rgba(17,24,39,0.6)] via-50% to-90% md:from-gray-900 md:via-[rgba(17,24,39,0.6)] md:to-gray-900 to-[rgba(17,24,39,0.6)]`}
       >
-        <div className='flex flex-col justify-end items-center h-2/5 lg:h-1/2 lg:px-20 xl:px-40'>
-          <h1 className='text-3xl md:text-4xl lg:text-6xl font-extrabold text-gray-300 mb-4 tracking-wider'>
+        d
+        <article className='flex flex-col justify-end items-center w-full mt-36 lg:mt-60'>
+          <h1 className='text-4xl lg:text-6xl font-extrabold text-gray-300 mb-4 tracking-wider'>
             Odkryj Filmi
             <span className='text-purple-500 font-bold'>X</span>
           </h1>
-          <p className='mb-10 text-xl md:text-2xl lg:text-3xl text-gray-300 tracking-tighter'>
+          <p className='text-xl md:text-2xl lg:text-3xl text-gray-300 tracking-tighter'>
             Najlepsze <span className='text-purple-500 font-bold'>filmy </span>i
             <span className='text-purple-500 font-bold'> seriale!</span>
           </p>
+        </article>
+        <div className='flex justify-between items-center flex-col w-full relative h-[14rem] lg:h-[20rem] pb-3'>
+          <InputSearch
+            setSearchResults={setSearchResults}
+            setSearchError={setSearchError}
+          />
+          {searchResults && (
+            <MovieTilesRow className={'items-center'} data={searchResults} />
+          )}
         </div>
-        <InputSearch />
       </div>
     </header>
   );
--- src/components/UI/InputSearch.js
+++ src/components/UI/InputSearch.js
@@ -1,26 +1,29 @@
-import { useRef, useState } from 'react';
-import axios from 'axios';
+import { useRef } from 'react';
+import { API_KEY } from '../../hooks/useMedia';
 import SearchIcon from './SearchIcon';
-import MovieTilesRow from './MovieTilesRow';
 
-const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+import axios from 'axios';
 
-const InputSearch = () => {
-  const [data, setData] = useState(null);
+const InputSearch = (props) => {
   const inputRef = useRef(null);
 
   const search = (query) => {
+    const API_URL_SEARCH = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${query}`;
     axios
-      .get(
-        `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
-      )
+      .get(API_URL_SEARCH)
       .then((res) => {
         const filteredData = res.data.results.filter(
           (result) => !result.backdropPath && result.backdrop_path
         );
-        setData(filteredData.slice(0, 3));
+        props.setSearchResults(filteredData.slice(0, 3));
       })
       .catch((err) => console.error(err.message));
+  };
+
+  const onClickHandler = () => {
+    const query = inputRef.current.value;
+    if (query.length === 0) return;
+    search(query);
   };
 
   const onSearchHandler = (e) => {
@@ -30,44 +33,35 @@ const InputSearch = () => {
     }
   };
 
-  const onClickHandler = () => {
-    const query = inputRef.current.value;
-    if (query.length === 0) return;
-    search(query);
-  };
-
   const onChangeHandler = () => {
     const query = inputRef.current.value;
     if (query.length === 0) {
-      setData(null);
+      props.setSearchResults(null);
       return;
     }
     search(query);
   };
 
   return (
-    <div className='relative flex items-center flex-col w-full h-3/5 lg:h-1/2'>
-      <div className='relative w-3/5 lg:w-2/5 flex justify-center items-center mt-3 md:mt-5 lg:mt-10'>
-        <input
-          className='px-4 py-2 pr-16 border border-purple-700 text-white bg-gray-900 rounded-full w-full'
-          type='text'
-          placeholder='Search...'
-          onKeyDown={onSearchHandler}
-          onChange={onChangeHandler}
-          ref={inputRef}
-        />
-        <figure
-          className='flex justify-center items-center bg-purple-700 h-full px-3 lg:px-4 py-1 absolute right-0 bottom-0 rounded-r-full cursor-pointer'
-          onClick={onClickHandler}
-        >
-          <SearchIcon />
-        </figure>
-      </div>
-      {data && (
-        <div className='w-screen pb-5 absolute bottom-0 md:w-4/5 '>
-          <MovieTilesRow data={data} />
-        </div>
-      )}
+    <div
+      className={`flex-center relative w-7/12 lg:w-2/5 2xl:w-1/4 mt-3 md:mt-5 lg:mt-10 mb-6 md:mb-10 2xl:mt-16 ${
+        props.className ? props.className : ''
+      }`}
+    >
+      <input
+        className='px-4 py-2 pr-16 border border-purple-700 text-white bg-gray-900 rounded-full w-full'
+        type='text'
+        placeholder='Search...'
+        onKeyDown={onSearchHandler}
+        onChange={onChangeHandler}
+        ref={inputRef}
+      />
+      <figure
+        className='flex-center bg-purple-700 h-full px-3 lg:px-4 py-1 absolute right-0 bottom-0 rounded-r-full cursor-pointer'
+        onClick={onClickHandler}
+      >
+        <SearchIcon />
+      </figure>
     </div>
   );
 };
