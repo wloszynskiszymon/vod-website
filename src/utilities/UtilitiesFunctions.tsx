3a49097f132088827997a2c70106efca2af3cb39
Szymon Włoszyński <66365050+wloszynskiszymon@users.noreply.github.com>
2024-09-08 14:08:07 +0200

    Migrated from js to ts

--- src/utilities/UtilitiesFunctions.tsx
+++ src/utilities/UtilitiesFunctions.tsx
@@ -1,19 +1,20 @@
 import MovieTile from '../components/MovieTile';
 import axios from 'axios';
 import { API_KEY } from '../hooks/useURL';
+import { FixMeLater } from '../types/types';
 
 // Array of images
-export const fetchBetterImages = async (data, mediaType = null) => {
-  const filteredRes = data.filter((data) => data.backdrop_path);
+export const fetchBetterImages = async (data: FixMeLater, mediaType = null) => {
+  const filteredRes = data.filter((data: FixMeLater) => data.backdrop_path);
 
-  const filteredData = filteredRes.map((data) => {
+  const filteredData = filteredRes.map((data: FixMeLater) => {
     return {
       ...data,
       media_type: data.media_type ? data.media_type : mediaType,
     };
   });
 
-  const axiosRequests = filteredData.map((singleData) => {
+  const axiosRequests = filteredData.map((singleData: FixMeLater) => {
     if (singleData.backdrop_path) {
       const imagesUrl = `https://api.themoviedb.org/3/${
         singleData.media_type ? singleData.media_type : mediaType
@@ -26,13 +27,13 @@ export const fetchBetterImages = async (data, mediaType = null) => {
   const axiosResponses = await axios.all(axiosRequests);
 
   const responsesWithBackdrop = axiosResponses.filter(
-    (res) => res.data.backdrops.length > 0
+    (res: FixMeLater) => res.data.backdrops.length > 0
   );
 
   // Map through the responses to get backdrop images
-  const imagesLinks = responsesWithBackdrop.map((imagesData) => {
-    let backdrops = imagesData.data.backdrops.find(
-      (backdrop) => backdrop.iso_639_1 === 'en' || null
+  const imagesLinks = responsesWithBackdrop.map((imagesData: FixMeLater) => {
+    let backdrops = imagesData?.data?.backdrops.find(
+      (backdrop: FixMeLater) => backdrop.iso_639_1 === 'en' || null
     );
 
     // If not found english one, take just the first
@@ -63,7 +64,7 @@ export const fetchBetterImages = async (data, mediaType = null) => {
 };
 
 // If movie is released for at least 3 months, return true
-export const formatTime = (minutes) => {
+export const formatTime = (minutes: FixMeLater) => {
   const hours = Math.floor(minutes / 60);
   const remainingMinutes = minutes % 60;
 
@@ -73,14 +74,14 @@ export const formatTime = (minutes) => {
   return hoursString + minutesString;
 };
 
-export function checkIsNew(dateString) {
+export function checkIsNew(dateString: FixMeLater) {
   const date = new Date(dateString);
   const diffInMillis = Date.now() - date.getTime();
   const months = diffInMillis / (1000 * 60 * 60 * 24 * 30);
   return months <= 3;
 }
 
-export const formatObjectData = (objArray, objProp) => {
+export const formatObjectData = (objArray: FixMeLater, objProp: FixMeLater) => {
   if (!objArray || !Array.isArray(objArray)) {
     return '';
   }
@@ -89,10 +90,10 @@ export const formatObjectData = (objArray, objProp) => {
   return dataStringArray.join(' | ');
 };
 
-export const renderMovieTiles = (mediaData) => {
-  const movieTiles = [];
+export const renderMovieTiles = (mediaData: FixMeLater) => {
+  const movieTiles: FixMeLater = [];
 
-  mediaData.forEach((singleData) => {
+  mediaData.forEach((singleData: FixMeLater) => {
     movieTiles.push(
       <div
         className='w-full h-full'
@@ -107,10 +108,10 @@ export const renderMovieTiles = (mediaData) => {
 
 // Handle scroll for infinite Scroll
 export const handleInfiniteScroll = (
-  scrollContainerRef,
-  isFetchingNextPage,
-  hasNextPage,
-  fetchNextPage
+  scrollContainerRef: FixMeLater,
+  isFetchingNextPage: FixMeLater,
+  hasNextPage: FixMeLater,
+  fetchNextPage: FixMeLater
 ) => {
   if (isFetchingNextPage) return;
   const container = scrollContainerRef.current;
