1dea21151059aef778d6d201c02b52c81475d89a
Szymon Włoszyński <66365050+wloszynskiszymon@users.noreply.github.com>
2024-10-13 13:27:17 +0200

    Test commit
    Test commit

--- src/services/providers/ReactQueryProvider.tsx
+++ src/services/providers/ReactQueryProvider.tsx
@@ -3,7 +3,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 
 const queryClient = new QueryClient();
-
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
