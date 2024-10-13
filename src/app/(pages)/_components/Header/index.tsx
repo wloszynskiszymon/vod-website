ac343f3ad051ef519d6f22758f280add699d50df
Szymon Włoszyński <66365050+wloszynskiszymon@users.noreply.github.com>
2024-10-13 15:47:42 +0200

    Removed cached Uppercase folders
    Removed cached Uppercase folders

--- src/app/(pages)/_components/Header/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as HeaderDescription } from "./Description";
-export { default as Header } from "./Header";
-export { default as HeaderTitle } from "./Title";
--- src/features/Poster/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as PosterBadge } from "./Badge";
-export { default as PosterRoot } from "./Root";
-export { default as PosterTitle } from "./Title";
