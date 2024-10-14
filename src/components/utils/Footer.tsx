a25954bf6deb0228e3945ea6d5b735c038c16d4f
Szymon Włoszyński <66365050+wloszynskiszymon@users.noreply.github.com>
2024-10-14 23:00:39 +0200

    Update Footer.tsx

--- src/components/utils/Footer.tsx
+++ src/components/utils/Footer.tsx
@@ -3,7 +3,7 @@ import Logo from "@/assets/Logo";
 const Footer = () => {
   return (
     <footer className="flex-center h-full w-full flex-col bg-gray-950 bg-gradient-to-r from-gray-950 via-[rgb(18,29,40)] to-gray-950 px-12 py-8">
-      <Logo className="h-32 w-auto scale-150" withSubtitle />
+      <Logo className="h-24 w-auto scale-150 md:h-32" withSubtitle />
       <a
         className="mb-2 border-b-2 border-blue-300 px-1 text-sm font-semibold tracking-tighter text-blue-300 drop-shadow-2xl transition duration-200 md:text-lg"
         href="https://developers.themoviedb.org/3"
