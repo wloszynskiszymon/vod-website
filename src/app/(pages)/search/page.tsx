74fd0621cd36bb44c25e659207265781e2f4f315
Szymon Włoszyński <66365050+wloszynskiszymon@users.noreply.github.com>
2024-10-14 22:41:28 +0200

    Fixed typo causing netlify build error
    Fixed typo causing netlify build error

--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -5,13 +5,12 @@ import SearchResults from "./_components/SearchResults";
 const SearchPage = () => {
   return (
     <div className="h-screen w-screen">
-      <SearchInput />
-
       <Suspense
         fallback={
           <div className="flex-center text-xl text-white">Loading...</div>
         }
       >
+        <SearchInput />
         <SearchResults />
       </Suspense>
     </div>
